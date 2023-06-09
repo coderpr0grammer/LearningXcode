ae259d7d5c910bcd2e0f5f1811d71c8636772aea
Daniel Martinez <daniellhf@icloud.com>
2023-06-08 20:17:55 -0400

    initial commit

--- LearningXcode/ViewController.swift
+++ LearningXcode/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var myButton: UIButton!
+    
+    @IBOutlet var mainLabel: UILabel!
+    
+    var lightOn = true
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        lightOn.toggle()
+        
+        mainView.backgroundColor = lightOn ? .white : .black
+        
+        mainLabel.textColor = lightOn ? .black : .white
+    }
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        super.viewDidLoad()        // Do any additional setup after loading the view.
+        
     }
 
 
